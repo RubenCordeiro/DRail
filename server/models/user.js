f0e4c7e29d7d4a0ac01de58f394425a3ea6753fc
RubenCordeiro <ruben.cordeiro.eng@gmail.com>
2015-10-14 19:41:22 +0100

    Server: add ticket information to the User model

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
 var db = require('seraph')(require('config').get('database'));
+var Sha1 = require('crypto-js/sha1');
 var model = require('seraph-model');
 
 var User = model(db, 'user');
@@ -10,5 +11,13 @@ User.schema = {
 
 var CreditCard = model(db, 'credit_card');
 User.compose(CreditCard, 'creditCards', 'owns');
+
+var Ticket = model(db, 'ticket'); // missing information: train, departure and destination stations
+Ticket.schema = {
+    id: { type: String, required: true },
+    date: { type: Date, required: true }
+};
+User.compose(Ticket, 'tickets', 'bought');
+
 
 module.exports = User;

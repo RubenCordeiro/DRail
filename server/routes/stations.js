1e88e9efa1c4b8a509fd47f6a3d1dd830bb9c0bb
RubenCordeiro <ruben.cordeiro.eng@gmail.com>
2015-10-20 19:14:07 +0100

    Add POST '/api/stations' route

--- server/routes/stations.js
+++ server/routes/stations.js
@@ -1,5 +1,9 @@
 var Boom = require('boom');
+var Joi = require('joi');
+
 var Station = require('../models/station');
+var db = require('seraph')(require('config').get('database'));
+
 
 module.exports = function (server) {
     server.route({
@@ -18,13 +22,50 @@ module.exports = function (server) {
     server.route({
         method: 'GET',
         path: '/api/stations/{name}',
-        handler: function(request, reply){
-            Station.where({ name: request.params.name }, function(err, station) {
+        handler: function (request, reply) {
+            Station.where({name: request.params.name}, function (err, station) {
                 if (err)
                     reply(Boom.badImplementation('Internal server error', err));
                 else
                     reply(station);
             });
+        }
+    });
+
+    server.route({
+        method: 'POST',
+        path: '/api/stations',
+        config: {
+            validate: {
+                payload: {
+                    name: Joi.string().min(3).max(20).required(),
+                    trips: Joi.array().items(Joi.object().keys({
+                        id: Joi.number().integer(),
+                        departureTime: Joi.date(),
+                        arrivalTime: Joi.date(),
+                        trainId: Joi.number().integer(),
+                        distance: Joi.number().integer()
+                    }))
+                }
+            }
+        },
+        handler: function (request, reply) {
+            var txn = db.batch();
+
+            var newStation = txn.save({name: request.payload.name});
+            txn.label(newStation, 'station');
+            if (request.payload.trips) {
+                request.payload.trips.forEach(function (trip) {
+                    txn.relate(newStation, 'trip', trip.id, {distance: trip.distance, departureTime: trip.departureTime, arrivalTime: trip.arrivalTime, trainId: trip.trainId});
+                });
+            }
+
+            txn.commit(function (err) {
+                if (err) return reply(Boom.badImplementation('Internal server error', err));
+
+                return reply(newStation);
+            });
+
         }
     });
 };

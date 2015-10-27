0f9b98ec4806265cfaa4527ef25c40de53b356dd
RubenCordeiro <ruben.cordeiro.eng@gmail.com>
2015-10-27 10:28:44 +0000

    Server: add documentation route

--- server/index.js
+++ server/index.js
@@ -1,9 +1,43 @@
 var Hapi = require('hapi');
 
 var server = new Hapi.Server();
-server.connection({port: 3000});
+server.connection({
+    port: 3000,
+    labels: ['api']
+});
 
 require('./routes')(server);
+
+server.register([
+    require('inert'),
+    require('vision'),
+    {
+        register: require('hapi-swaggered'),
+        options: {
+            tags: {
+                'foobar/test': 'Example foobar description'
+            },
+            info: {
+                title: 'Example API',
+                description: 'Powered by node, hapi, joi, hapi-swaggered, hapi-swaggered-ui and swagger-ui',
+                version: '1.0'
+            }
+        }
+    },
+    {
+        register: require('hapi-swaggered-ui'),
+        options: {
+            title: 'Example API',
+            path: '/docs',
+            swaggerOptions: {} // see above
+        }
+    }], {
+    select: 'api'
+}, function (err) {
+    if (err) {
+        throw err
+    }
+});
 
 server.start(function () {
     console.log('Server running at:', server.info.uri);
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,13 @@
 module.exports = function(server) {
+
+    server.route({
+        path: '/',
+        method: 'GET',
+        handler: function (request, reply) {
+            reply.redirect('/docs')
+        }
+    });
+
     require('./stations')(server);
     require('./trips')(server);
 };

4073787f6bca0c9a9e3dfe39f1268536d6f7166c
RubenCordeiro <ruben.cordeiro.eng@gmail.com>
2015-10-27 11:35:32 +0000

    Server: update trip.find: format return data

--- server/models/trip.js
+++ server/models/trip.js
@@ -1,3 +1,5 @@
+var Lazy = require('lazy.js');
+
 var db = require('seraph')(require('config').get('database'));
 
 module.exports = {
@@ -16,13 +18,39 @@ module.exports = {
     },
 
     find: function (departureStationId, arrivalStationId, callback) {
-        db.query('MATCH path = (departureStation:station)-[:trip*]-(arrivalStation:station) WHERE ID(departureStation) = {departureStationId} AND ID(arrivalStation) = {arrivalStationId} RETURN EXTRACT( n in nodes(path) | n) as nodes_in_path, EXTRACT(r in relationships(path) | r), path, length(path)', {
+        db.query('MATCH path = (departureStation:station)-[:trip*]-(arrivalStation:station) WHERE ID(departureStation) = {departureStationId} AND ID(arrivalStation) = {arrivalStationId} RETURN EXTRACT( n in nodes(path) | n) as stations, EXTRACT(r in relationships(path) | r) as trips, path, length(path)', {
             departureStationId: departureStationId,
             arrivalStationId: arrivalStationId
         }, function (err, trips) {
             if (err) return callback(err, null);
 
-            return callback(null, trips);
+
+            var indexedSations = Lazy(trips)
+                .map(function (elem) {
+                    return elem.stations;
+                })
+                .flatten()
+                .indexBy('id');
+
+            var ret = Lazy(trips)
+                .map(function (elem) {
+                    return Lazy(elem.trips)
+                        .map(function (trip) {
+                            return {
+                                start: indexedSations.get(trip.start),
+                                end: indexedSations.get(trip.end),
+                                trainId: trip.properties.trainId,
+                                departureTime: trip.properties.departureTime,
+                                arrivalTime: trip.properties.arrivalTime,
+                                distance: trip.properties.distance
+                            }
+                        })
+                        .toArray();
+                })
+                .flatten();
+
+
+            return callback(null, ret.toArray());
         });
     },
 

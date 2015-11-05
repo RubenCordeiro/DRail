59aade010cdf55dd0df252de0c91ca99099ebe3a
Ruben Cordeiro <ruben.cordeiro.eng@gmail.com>
2015-11-05 22:46:56 +0000

    Server: add POST /api/trips route

--- server/routes/trips.js
+++ server/routes/trips.js
@@ -28,4 +28,33 @@ module.exports = function(server) {
         }
     });
 
+    server.route({
+        method: 'POST',
+        path: '/api/trips',
+        config: {
+            auth: false,
+            validate: {
+                payload: {
+                    from: Joi.number().integer().required(),
+                    to: Joi.number().integer().required(),
+                    distance: Joi.number().integer().required(),
+                    departureDate: Joi.date().required(),
+                    arrivalDate: Joi.date().required(),
+                    trainId: Joi.number().integer().required()
+                }
+            },
+            tags: ['api']
+        },
+        handler: (request, reply) => {
+            var payload = request.payload;
+
+            Trip.save(payload.from, payload.to, payload.departureDate, payload.arrivalDate, payload.trainId, payload.distance,
+                (err, trip) => {
+                    if (err) return reply(Boom.badImplementation('Internal server error'));
+
+                    return reply(trip);
+            });
+        }
+    });
+
 };

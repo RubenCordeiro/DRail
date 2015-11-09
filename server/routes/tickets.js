50f3621e8084c252ea022a572d03908e76351e2d
Ruben Cordeiro <ruben.cordeiro.eng@gmail.com>
2015-11-09 10:12:06 +0000

    Server: update POST /api/user/{userId}/tickets

--- server/routes/tickets.js
+++ server/routes/tickets.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const Boom = require('boom'),
+const Async = require('async'),
+    Boom = require('boom'),
     Joi = require('joi'),
     Lazy = require('lazy.js');
 
@@ -113,9 +114,10 @@ module.exports = (server) => {
                     userId: Joi.number().integer().required()
                 },
                 payload: {
-                    date: Joi.date().required(),
-                    departureStation: Joi.number().integer().required(),
-                    arrivalStation: Joi.number().integer().required()
+                    trips: Joi.array().items(Joi.object().keys({
+                        trainId: Joi.number().integer().required(),
+                        id: Joi.number().integer().required()
+                    })).required()
                 }
             },
             tags: ['api', 'tickets']
@@ -133,43 +135,44 @@ module.exports = (server) => {
                     return reply(Boom.notFound('User ID does not exist'));
                 }
 
-                server.inject('/api/trips?from=' + request.payload.departureStation + '&to=' + request.payload.arrivalStation
-                    + '&shortest=true',
-                    (res) => {
+                var currentDate = new Date();
+                var currentTrainId = Lazy(request.payload.trips).first().trainId;
+                var requestedTrips = Lazy(request.payload.trips);
 
-                        if (!res.result || res.result.length === 0)
-                            return reply(Boom.notFound('No valid trip was found'));
+                Async.whilst(
+                    () => requestedTrips.length() != 0,
+                    (callback) => {
+                        var consecutiveTrips = requestedTrips.takeWhile((trip) => {
+                            if (trip.trainId != currentTrainId) {
+                                currentTrainId = trip.trainId;
+                                return false;
+                            } else
+                                return true;
+                        }).toArray();
 
-                        var trip = Lazy(res.result).first();
-
-                        var reducer = (path, nextTrip) => {
-                            if (nextTrip.start.isCentral) {
-                                return path.concat([nextTrip]);
+                        User.push(user.id, 'tickets', {
+                            creationDate: currentDate,
+                            trips: consecutiveTrips,
+                            status: 'pending'
+                        }, (err) => {
+                            if (err) {
+                                return callback(err);
+                            } else {
+                                return callback(null);
                             }
-                            else {
-                                path[path.length - 1].push(nextTrip);
-                                return path;
-                            }
-                        };
+                        });
 
-                        var ticketPaths = trip.reduce(reducer, [[]]).filter((path) => path.length > 0)
-                            .map((subpath) => subpath.map((segment) => segment.tripId));
+                        requestedTrips = requestedTrips.without(consecutiveTrips);
+                    },
+                    (err) => {
+                        if (err) {
+                            server.log(['error', 'database'], err);
+                            return reply(Boom.badImplementation('Internal server error'));
+                        }
 
-                        var ticketDate = new Date();
-                        Lazy(ticketPaths).async().each(subpath => {
-                                User.push(user.id, 'tickets', { creationDate: ticketDate, trips: subpath, status: 'pending' }, (err) => {
-                                    if (err) {
-                                        server.log(['error', 'database'], err);
-                                        throw err;
-                                    }
-                                });
-                            })
-                            .onComplete(() => reply('Ticket created successfully').code(201))
-                            .onError(err => { // any exception is caught here
-                                server.log(['error', 'database'], err);
-                                reply(Boom.badImplementation('Internal server error'));
-                            });
-                    });
+                        return reply('Tickets were created successfully');
+                    }
+                );
             });
         }
     });

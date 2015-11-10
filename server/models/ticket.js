86f9893ff352e73abc286456888e241a135f5047
DDuarte <dnpd.dd@gmail.com>
2015-11-10 23:34:01 +0000

    Server: actually generate a ticket signature with RSA and SHA1

--- server/models/ticket.js
+++ server/models/ticket.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const db = require('seraph')(require('config').get('database'));
-const Sha1 = require('crypto-js/sha1');
+const jsrsasign = require('jsrsasign');
 const model = require('seraph-model');
+
+const prvKeyPEM = "-----BEGIN RSA PRIVATE KEY-----\nMIIBywIBAAJhAMxmnGhwRwo84+v1NGUDXy+uoN14ckd9iKvmFC5Sb6V50JKQZ9rX\noosv3UBknKYh0KdotYcArbXjMjmpMrBlh0t8orkgr9h6egPKiSaf9IOuirV+mvPW\nWh4xVsNMdePP2QIDAQABAmAd/OPvkqFpiBtsT9I7C66YYUdqlrQ1dt5pUd0eGqwU\nm/WUuyjxe3d2cjREsT4mRYmDCt/A/YUX2pV/9YGuJvM6fgajAXDrMmK7PRomSWMh\nOhC2YZ40RCrmFhUMJnRfI50CMQDtiGnVlyjN5yD8MPqESKtmjiYWvf10jejiIlgS\na4ziyT7ho8SurKj9/iwBASepdfsCMQDcSsZU6M+2caksxoiJLVs67HyNuO/71q7a\n5FqhW0Qsl5sa6IiHLuEfv2yfiPthrTsCMQC6gikvyAAHJNt2ifK62eCTpzvrEYUo\n9qCMpxDmbcJy9DfGrnOp//K1dNUSeNiuq+8CMQC+jC9h4r9IoKglAtW3UnRHU6ep\nLv7mZ5x32m5KhBdEOx7+94Sg6fvw6jObC3Hl22ECMF7E0LHpiwbJxUgpHTcJrB0s\nCQQplNAB9mjSYgtb6fTWqL5IL27ClMTdXqqWZ8R1dw==\n-----END RSA PRIVATE KEY-----"
 
 var Ticket = model(db, 'ticket');
 Ticket.schema = {
@@ -11,7 +13,10 @@ Ticket.schema = {
     status: { type: String, required: true, default: 'pending'}
 };
 Ticket.addComputedField('signature', function(ticket) {
-    return Sha1(ticket.id.toString() + ticket.creationDate).toString();
+    var sig = new jsrsasign.crypto.Signature({"alg": "SHA1withRSA"});
+    sig.init(prvKeyPEM);
+    sig.updateString(ticket.id.toString() + ticket.creationDate);
+    return sig.sign();
 });
 
 var Train = require('./train');

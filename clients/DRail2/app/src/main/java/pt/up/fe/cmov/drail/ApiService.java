f6d98cf51642ef5c95664c2d226f1a7117631a00
Ruben Cordeiro <ruben.cordeiro.eng@gmail.com>
2015-11-10 00:24:58 +0000

    Client: add base graph drawing functionality

--- clients/DRail2/app/src/main/java/pt/up/fe/cmov/drail/ApiService.java
+++ clients/DRail2/app/src/main/java/pt/up/fe/cmov/drail/ApiService.java
@@ -1,25 +1,60 @@
 package pt.up.fe.cmov.drail;
 
-import java.io.Serializable;
-import java.util.List;
+import java.util.ArrayList;
 
 import retrofit.Call;
 import retrofit.GsonConverterFactory;
 import retrofit.Retrofit;
 import retrofit.http.GET;
 import retrofit.http.Header;
-import retrofit.http.Query;
 
 public final class ApiService {
 
-    public static final String API_URL = "http://192.168.56.1:3000";
+    public static final String API_URL = "http://192.168.1.135:3000";
 
     public static final ApiService.DRail service = new Retrofit.Builder()
             .baseUrl(ApiService.API_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build().create(ApiService.DRail.class);
 
-    public interface DRail {
+    public static class Station {
+        public final int id;
+        public final String name;
+        public final boolean isCentral;
+        public final double longitude;
+        public final double latitude;
 
+        public Station(int id, String name, boolean isCentral, double longitude, double latitude) {
+            this.id = id;
+            this.name = name;
+            this.isCentral = isCentral;
+            this.longitude = longitude;
+            this.latitude = latitude;
+        }
+    }
+
+    public static class GraphEdge {
+        public final int start;
+        public final int end;
+
+        public GraphEdge(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+    }
+
+    public static class Graph {
+        public final ArrayList<Station> stations;
+        public final ArrayList<GraphEdge> trips;
+
+        public Graph(ArrayList<Station> stations, ArrayList<GraphEdge> trips) {
+            this.stations = stations;
+            this.trips = trips;
+        }
+    }
+
+    public interface DRail {
+        @GET("/api/graph")
+        Call<Graph> getGraph(@Header("Bearer") String bearer);
     }
 }
--- clients/DRail2/app/src/main/java/pt/up/fe/cmov/drail/SchedulingFragment.java
+++ clients/DRail2/app/src/main/java/pt/up/fe/cmov/drail/SchedulingFragment.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.InflateException;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,6 +17,14 @@ import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.HashMap;
+
+import retrofit.Call;
+import retrofit.Callback;
+import retrofit.Response;
+import retrofit.Retrofit;
 
 
 /**
@@ -75,14 +84,45 @@ public class SchedulingFragment extends Fragment {
             view = inflater.inflate(R.layout.fragment_scheduling, container, false);
             map = ((SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map))
                     .getMap();
-            Circle circle = map.addCircle(new CircleOptions()
-                    .center(new LatLng(-33.87365, 151.20689))
-                    .radius(10000)
-                    .strokeColor(Color.RED)
-                    .fillColor(Color.BLUE));
+
         } catch (InflateException e) {
         /* map is already there, just return view as it is */
         }
+
+        Call<ApiService.Graph> graphRequest = ApiService.service.getGraph("token");
+        graphRequest.enqueue(new Callback<ApiService.Graph>() {
+            @Override
+            public void onResponse(Response<ApiService.Graph> response, Retrofit retrofit) {
+                if (response.isSuccess()) {
+                    ApiService.Graph graph = response.body();
+
+                    HashMap<Integer, ApiService.Station> indexedStations = new HashMap<>();
+                    for (ApiService.Station s: graph.stations) {
+                        indexedStations.put(s.id, s);
+                        map.addCircle(new CircleOptions()
+                                .center(new LatLng(s.latitude, s.longitude))
+                                .radius(10000)
+                                .fillColor(Color.BLUE));
+                    }
+
+                    ApiService.Station source, target;
+                    for (ApiService.GraphEdge e: graph.trips) {
+                        source = indexedStations.get(e.start);
+                        target = indexedStations.get(e.end);
+                        map.addPolyline(new PolylineOptions()
+                        .add(new LatLng(source.latitude, source.longitude), new LatLng(target.latitude, target.longitude))
+                                .width(5).color(Color.RED));
+                    }
+                } else {
+                    Log.d("Error", response.raw().request().urlString());
+                }
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                Log.d("Error", t.getMessage());
+            }
+        });
         return view;
     }
 

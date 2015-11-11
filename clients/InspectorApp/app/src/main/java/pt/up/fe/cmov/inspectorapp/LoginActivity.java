b5278163f69e448cefd1c733a1668ecbe5b1e0a0
DDuarte <dnpd.dd@gmail.com>
2015-11-11 15:41:58 +0000

    InspectorApp: Re-enable login in InspectorApp
    Use inspector@drail (pw drail)

--- clients/InspectorApp/app/src/main/java/pt/up/fe/cmov/inspectorapp/LoginActivity.java
+++ clients/InspectorApp/app/src/main/java/pt/up/fe/cmov/inspectorapp/LoginActivity.java
@@ -52,7 +52,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * TODO: remove after connecting to a real authentication system.
      */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world", "a@a.com:hello"
+            "foo@example.com:hello", "bar@example.com:world", "a@a.com:hello", "inspector@drail:drail"
     };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
@@ -194,11 +194,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     }
 
     private boolean isEmailValid(String email) {
-        return true; // return !TextUtils.isEmpty(email) && email.contains("@");
+       return !TextUtils.isEmpty(email) && email.contains("@");
     }
 
     private boolean isPasswordValid(String password) {
-        return true; // return !TextUtils.isEmpty(password) && password.length() > 4;
+        return !TextUtils.isEmpty(password) && password.length() > 4;
     }
 
     /**
@@ -307,11 +307,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         @Override
         protected Boolean doInBackground(Void... params) {
-            // TODO: attempt authentication against a network service.
 
             try {
-                // Simulate network access.
-                Thread.sleep(1);
+                Thread.sleep(500);
             } catch (InterruptedException e) {
                 return false;
             }
@@ -324,7 +322,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 }
             }
 
-            // TODO: register the new account here.
             return true;
         }
 

0706d4e7148d8acabcc4bcdf20380b03eb6706e9
Ruben Cordeiro <ruben.cordeiro.eng@gmail.com>
2015-11-13 16:04:42 +0000

    DRail2: add login and register activities

---
+++ clients/DRail2/app/src/main/java/pt/up/fe/cmov/drail/RegisterActivity.java
@@ -0,0 +1,164 @@
+package pt.up.fe.cmov.drail;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.app.LoaderManager.LoaderCallbacks;
+
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit.Call;
+import retrofit.Callback;
+import retrofit.Response;
+import retrofit.Retrofit;
+
+import static android.Manifest.permission.READ_CONTACTS;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class RegisterActivity extends AppCompatActivity {
+
+    /**
+     * Id to identity READ_CONTACTS permission request.
+     */
+    private static final int REQUEST_READ_CONTACTS = 0;
+
+    // UI references.
+    private View mProgressView;
+    private View mLoginFormView;
+
+    private TextView mNameRegisterTextView;
+    private TextView mUsernameRegisterTextView;
+    private TextView mPasswordRegisterTextView;
+    private TextView mCcRegisterTextView;
+    private Button mRegisterButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        // Set up the login form.
+        mNameRegisterTextView = (TextView) findViewById(R.id.register_name);
+        mUsernameRegisterTextView = (TextView) findViewById(R.id.register_username);
+        mPasswordRegisterTextView = (TextView) findViewById(R.id.register_password);
+        mCcRegisterTextView = (TextView) findViewById(R.id.register_cc);
+        mRegisterButton = (Button) findViewById(R.id.register_button);
+
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                attemptLogin();
+            }
+        });
+
+        mLoginFormView = findViewById(R.id.login_form);
+        mProgressView = findViewById(R.id.login_progress);
+    }
+
+
+
+    /**
+     * Attempts to sign in or register the account specified by the login form.
+     * If there are form errors (invalid email, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+    private void attemptLogin() {
+
+
+        mNameRegisterTextView.setError(null);
+        mUsernameRegisterTextView.setError(null);
+        mPasswordRegisterTextView.setError(null);
+        mCcRegisterTextView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String name = mNameRegisterTextView.getText().toString();
+        String username = mUsernameRegisterTextView.getText().toString();
+        String password = mPasswordRegisterTextView.getText().toString();
+        String cc = mCcRegisterTextView.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid password, if the user entered one.
+        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+            mPasswordRegisterTextView.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordRegisterTextView;
+            cancel = true;
+        }
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(username)) {
+            mUsernameRegisterTextView.setError(getString(R.string.error_field_required));
+            focusView = mUsernameRegisterTextView;
+            cancel = true;
+        }
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(name)) {
+            mNameRegisterTextView.setError(getString(R.string.error_field_required));
+            focusView = mNameRegisterTextView;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            Call<ApiService.LoginUserResponse> registerRequest = ApiService.service.register(new ApiService.RegisterUserRequest(name, username, password, cc));
+            registerRequest.enqueue(new Callback<ApiService.LoginUserResponse>() {
+                @Override
+                public void onResponse(Response<ApiService.LoginUserResponse> response, Retrofit retrofit) {
+                    if (response.isSuccess()) { // successful request, build graph
+                        MainActivity.mLoginUser = response.body();
+                        Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                        setIntent(intent);
+                    } else {
+                        Log.d("Error", response.raw().request().urlString());
+                    }
+                }
+
+                @Override
+                public void onFailure(Throwable t) {
+
+                }
+            });
+        }
+    }
+
+    private boolean isPasswordValid(String password) {
+        //TODO: Replace this with your own logic
+        return password.length() > 4;
+    }
+}
---
+++ clients/DRail2/app/src/main/java/pt/up/fe/cmov/drail/SignInActivity.java
@@ -0,0 +1,164 @@
+package pt.up.fe.cmov.drail;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.app.LoaderManager.LoaderCallbacks;
+
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit.Call;
+import retrofit.Callback;
+import retrofit.Response;
+import retrofit.Retrofit;
+
+import static android.Manifest.permission.READ_CONTACTS;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class SignInActivity extends AppCompatActivity {
+
+
+    // UI references.
+    private AutoCompleteTextView mUsernameView;
+    private EditText mPasswordView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        // Set up the login form.
+        mUsernameView = (AutoCompleteTextView) findViewById(R.id.email);
+
+        mPasswordView = (EditText) findViewById(R.id.password);
+        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == R.id.login || id == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+        mEmailSignInButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin();
+            }
+        });
+
+        // I can't access the go_to_register_button... I am clueless
+        /*Button registerButton = (Button) findViewById(R.id.go_to_register_button);
+        registerButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });*/
+    }
+
+    /**
+     * Attempts to sign in or register the account specified by the login form.
+     * If there are form errors (invalid email, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+    private void attemptLogin() {
+
+
+        // Reset errors.
+        mUsernameView.setError(null);
+        mPasswordView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String username = mUsernameView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid password, if the user entered one.
+        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+            mPasswordView.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordView;
+            cancel = true;
+        }
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(username)) {
+            mUsernameView.setError(getString(R.string.error_field_required));
+            focusView = mUsernameView;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+
+            Call<ApiService.LoginUserResponse> graphRequest = ApiService.service.login(new ApiService.LoginUserRequest(username, password));
+            graphRequest.enqueue(new Callback<ApiService.LoginUserResponse>() {
+                @Override
+                public void onResponse(Response<ApiService.LoginUserResponse> response, Retrofit retrofit) {
+                    if (response.isSuccess()) { // successful request, build graph
+                        MainActivity.mLoginUser = response.body();
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
+                    } else {
+                        Log.d("Error", response.raw().request().urlString());
+                    }
+                }
+
+                @Override
+                public void onFailure(Throwable t) {
+
+                }
+            });
+        }
+    }
+
+    private boolean isEmailValid(String email) {
+        //TODO: Replace this with your own logic
+        return email.contains("@");
+    }
+
+    private boolean isPasswordValid(String password) {
+        //TODO: Replace this with your own logic
+        return password.length() > 4;
+    }
+}
+
